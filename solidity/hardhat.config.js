f9385ce7a02cce5f5f005325626e1d47a6a6e96c
amenehmn <ameneh.mn@gmail.com>
2022-07-11 14:08:52 +0430

    Add files via upload

---
+++ solidity/hardhat.config.js
@@ -0,0 +1,18 @@
+require('@nomiclabs/hardhat-waffle');
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+
+module.exports = {
+  solidity: '0.8.1',
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETERSCAN_API_KEY,
+  }
+};
